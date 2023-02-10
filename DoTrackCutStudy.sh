b0a92d5831c77fd3894cdacf9645f04abb696648
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-02-10 18:43:04 -0500

    Add script to run driver macro

---
+++ DoTrackCutStudy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 'DoTrackCutStudy.sh'
+# Derek Anderson
+# 02.10.2023
+#
+# A script to run 'DoTrackCutStudy.C'
+
+root -b -q DoTrackCutStudy.C
+
+# end -------------------------------------------------------------------------
